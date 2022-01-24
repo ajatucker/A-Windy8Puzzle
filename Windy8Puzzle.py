6d28abc27b183010b16d1ce69b36b95b01131078
ajatucker <ajatucker@outlook.com>
2022-01-24 18:19:12 -0500

    Made graph class into a digraph

--- Windy8Puzzle.py
+++ Windy8Puzzle.py
@@ -31,8 +31,6 @@ class pq:
             print()
          
 
-
-
 heapObj = pq()
 heapObj.insert(3)
 heapObj.insert(2)
@@ -44,9 +42,9 @@ heapObj.remove()
    
 #print(heapObj.peek())
 
-heapObj.printHeap()
+#heapObj.printHeap()
 
-print(heapObj.isEmpty())
+#print(heapObj.isEmpty())
 
 #heapObj.remove()
 #heapObj.remove()
@@ -54,18 +52,15 @@ print(heapObj.isEmpty())
 #heapObj.printHeap()
 
 class aGraph:
-   def __init__(self,gDict=None):
-      if gDict is None:
-         gDict = {}
-      self.gDict = gDict
-   def getVertices(self):
-      return list(self.gDict.keys())
-   def getEdges(self):
-      return self.findEdges()
-#Add vertex
+   def __init__(self, edges, n):
+      self.adj = [[] for _ in range(n)]
+      
+      for(s, d) in edges:
+         self.adj[s].append(d)
+#Add vertex to adjacency list
    def addVertex(self, v):
-      if v not in self.gDict:
-         self.gDict[v] = []
+      if v not in self.adj:
+         self.adj[v] = []
 #Add an edge
    def addEdge(self, edge):
       edge = set(edge)
@@ -74,6 +69,17 @@ class aGraph:
          self.gDict[v1].append(v2)
       else:
          self.gDict[v1] = [v2]
+   def swapVertex(self, v1, v2):
+      swapped = False
+      if self.adj[v1] != 0 and self.adj[v2] != 0:
+         x = self.adj[v1]
+         y = self.adj[v2]
+         self.adj[v2] = x
+         self.adj[v1] = y
+         #self.adj[v1][x] = [v2]
+         swapped = True
+      return swapped
+            
 #List the edges of graph
    def findEdges(self):
       edgename = []
@@ -82,6 +88,15 @@ class aGraph:
             if {nxtvx, vx} not in edgename:
                edgename.append({vx, nxtvx})
       return edgename
+#print the adjacency matrix
+   def printGraph(self):
+    for src in range(len(self.adj)):
+        for dest in self.adj[src]:
+            if dest == 0 or src == 0:
+               print(f'( - —> {dest}) ', end='')
+            else:
+               print(f'({src} —> {dest}) ', end='')
+        print()
 
 #initial state
 initArr = [[2, 8, 3], [6, 7, 4], [1, 5, 0]]
@@ -95,22 +110,19 @@ goalArr = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
 #7, 6, 5
 
 # Create the dictionary with graph elements
-initialSt = { 
-   "2" : ["8","6"],
-   "8" : ["2", "3", "7"],
-   "3" : ["8", "4"],
-   "6" : ["7", "1"],
-   "7" : ["6", "8", "4", "5"],
-   "4" : ["7", "3", "0"] ,
-   "1" : ["6", "5"],
-   "5" : ["1", "7", "0"],
-   "0" : ["5", "4"]
-}
+initialSt = [(2, 8), (2, 6), (8, 2), (8, 3),(8,7), (3,8), (3,4), (7, 6), (7,8), (7, 4), (7,5), (4, 7), (4, 3), (4, 0),
+ (1, 6), (1,5), (5, 1), (5, 7), (5,0), (0,5), (0,4)]
 
-g = aGraph(initialSt)
+numOfVertices = 9
+
+g = aGraph(initialSt, numOfVertices)
 #print(g.getVertices())
 #print(g.getEdges())
 
-#print(goalArr)
+#g.printGraph()
+
+print(g.swapVertex(1, 2))
+
+g.printGraph()
 
 
